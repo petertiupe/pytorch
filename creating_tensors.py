0be103a4da02f0fa45542f8f5aecf0d9b6a2cd72
Peter Marx <peter@tiupe.de>
2025-08-31 11:55:09 +0200

    Tensoren mit pytorch

---
+++ creating_tensors.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import torch
+
+# So werden Tensoren mit dem entsprechenden Rang erzeugt. Der Rang einer Matrix hat nichts mit
+# der Dimension von zugehörigen Vektoren zu tun.
+tensor0d = torch.tensor(1)
+tensor1d = torch.tensor([1,2,3]) # Vektordimension ist 3 Rang des Tensors ist 1
+tensor2d = torch.tensor([[1,2,3], [4,5,6]]) # 2x3 - Matrix vom Rang 2
+
+print(tensor0d.shape)
+print(tensor1d.shape)
+print(tensor2d.shape)
+
+tensor2dT = tensor2d.T # transformiert die Matrix
+print(tensor2dT)
+
+# Matrizenmultiplikation
+matrix_multiplication = tensor2d.matmul(tensor2dT)
+print(matrix_multiplication)
+
+# Matrizenmultiplikation kann man auch mit einem @ schreiben
+matrix_with_at = tensor2d @ tensor2dT
+print(matrix_with_at)
+
+# Umformen einer Matrix
+viewed = tensor2d.view(1,6)
+print(viewed)
--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import torch
+# So kann man testen, ob die GPU bei Berechnungen auf dem Mac benutzt werden kann. 
+# Bei mir ist das Ergebnis true 
 print(torch.backends.mps.is_available())
 print(torch.__version__)
 print("Diese Zeile wird ausgegeben!")
