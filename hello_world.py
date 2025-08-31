24cac3ff137935bedfbb313688a6140315fdd95e
Peter Marx <peter@tiupe.de>
2025-08-31 15:11:12 +0200

    partielle_ableitung Datei neu hinzugefügt

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
+import sys
 import torch
+import torchvision
+
+print("Python-Version: ", sys.version)
+
 # So kann man testen, ob die GPU bei Berechnungen auf dem Mac benutzt werden kann. 
 # Bei mir ist das Ergebnis true 
 print(torch.backends.mps.is_available())
-print(torch.__version__)
-print("Diese Zeile wird ausgegeben!")
+
+print("torch-Version: ", torch.__version__)
+
+
+print("torchvision-Version: ", torchvision.__version__)
+
+
+x = torch.rand(5, 3)
+print("Zufällige 5x3-Matrix:\n", x)
